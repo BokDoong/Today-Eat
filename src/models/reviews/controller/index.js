d8a1c98c3edf608ef5e53c05162ccea69bccd9d3
junhyeok <junhyeok0246@naver.com>
2023-07-19 16:26:33 +0900

    리뷰 작성 API 설계

---
+++ src/models/reviews/controller/index.js
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import { ReviewService } from "../service";
+import { CreateReviewDTO } from "../dto";
+import {imageUploader} from "../../../middleware";
+
+class ReviewController{
+    router;
+    path = "/api/v1/reviews";
+    reviewService;
+
+    constructor(){
+        this.router = new Router();
+        this.reviewService = new ReviewService();
+        this.init();
+    }
+
+    init(){
+        this.router.post("/",imageUploader.array('images'),this.createReview.bind(this));
+    }
+    
+    //리뷰 작성
+    createReview = async (req,res,next) => {
+        try{
+            const filePaths = req.files.map(file => process.env.AWS_S3_BUCKET + ".s3." + process.env.AWS_S3_REGION + ".amazonaws.com/" + file.key);
+            const body = JSON.parse(req.body['dto']);
+
+            const newReviewId = await this.reviewService.createReview(
+                new CreateReviewDTO({
+                    userId:req.user.id,
+                    storeId:body.storeId,
+                    content:body.content,
+                    score:body.score,
+                    tags:body.tags,
+                    keywords:body.keywords,
+                    images:filePaths,
+                })
+            )
+            res.status(201).json({id:newReviewId});
+        }
+        catch(err){
+            next(err);
+        }
+    }
+}
+
+const reviewController = new ReviewController();
+export default reviewController;
---
+++ src/models/reviews/service/index.js
@@ -0,0 +1,52 @@
+import {UserService} from "../../users/service";
+import {StoreService} from "../../stores/service";
+import database from "../../../database";
+
+export class ReviewService{
+    userService;
+    storeService;
+    constructor(){
+        this.userService = new UserService();
+        this.storeService = new StoreService();
+    }
+
+
+    //리뷰 작성
+    createReview = async (props) => {
+        const user = await this.userService.findUserById(props.userId);
+        const store = await this.storeService.findStoreByID(props.storeId);
+        const newReview = await database.review.create({
+            data:{
+                content:props.content,
+                score:props.score,
+                user:{
+                    connect:{
+                        id:user.id,
+                    }
+                },
+                store:{
+                    connect:{
+                        id:store.id,
+                    },
+                },
+                keywords:{
+                    createMany:{
+                        data:props.keywords.map((keyword)=>({name:keyword}))
+                    }
+                },
+                tags:{
+                    createMany:{
+                        data:props.tags.map((tag)=>({name:tag}))
+                    }
+                },
+                reviewImages:{
+                    createMany:{
+                        data:props.images.map((image)=>({imageUrl:image}))
+                    }
+                }
+            }
+        })
+
+        return newReview.id;
+    }
+}
