511b6b72d656d46c1ab5a017b3e336921703f22c
junhyeok <junhyeok0246@naver.com>
2023-07-28 15:11:49 +0900

    가게 검색 API 설계

---
+++ src/models/stores/dto/store-search.dto.js
@@ -0,0 +1,21 @@
+export class StoreSearchDTO{
+    id;
+    name;
+    category;
+    distance
+    status;
+    score;
+    reviewCount;
+    imageUrl;
+
+    constructor(props) {
+        this.id = props.id;
+        this.name = props.name;
+        this.category = props.category;
+        this.imageUrl = props.imageUrl;
+        this.distance = props.distance;
+        this.status = props.status;
+        this.score = props.score;
+        this.reviewCount = props.reviewCount;
+    }
+}
