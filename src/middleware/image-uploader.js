493b677a108d7d34457cd20d561e041d768d3a53
junhyeok <junhyeok0246@naver.com>
2023-08-03 13:15:16 +0900

    리뷰 수정,삭제 시 S3 이미지 삭제 기능

--- src/middleware/image-uploader.js
+++ src/middleware/image-uploader.js
@@ -1,42 +1,45 @@
-import { S3Client } from '@aws-sdk/client-s3'
 import multer from 'multer'
 import multerS3 from 'multer-s3'
-import path from 'path'
+import path from 'path';
+import AWS from 'aws-sdk';
 
-const s3 = new S3Client({
-  region: process.env.AWS_S3_REGION,
-  credentials: {
+export const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-  signatureVersion: 'v4',
-})
+    region: process.env.AWS_S3_REGION
+  });
+  
 
-// 확장자 목록
 const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp', '.gif'];
 
 export const imageUploader = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.AWS_S3_BUCKET,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: 'public-read',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
-    },
-    key: function (req, file, cb) {
-      // 확장자 검사
-      const extension = path.extname(file.originalname).toLowerCase();
-      if(!allowedExtensions.includes(extension)) {
-        return cb(new Error('확장자 에러'));
+  storage : multerS3({
+      s3:s3,
+      bucket:process.env.AWS_S3_BUCKET,
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname});
+      },
+      acl : 'public-read-write',
+      key : function (req, file, cb) {
+        // 확장자 검사
+        const extension = path.extname(file.originalname).toLowerCase();
+        if(!allowedExtensions.includes(extension)) {
+          return cb(new Error('확장자 에러'));
+        }
+  
+        cb(null, Date.now().toString());
       }
-
-      cb(null, Date.now().toString());
-    },
   }),
   limits: {
     fileSize: 50 * 1024 * 1024 // 용량제한: 50MB
   }
-})
+});
+
+export const deleteImage = (filename) => {
+  s3.deleteObject({
+    Bucket : process.env.AWS_S3_BUCKET,
+    Key: filename,
+    }, function(err, data){}
+  );
+}
