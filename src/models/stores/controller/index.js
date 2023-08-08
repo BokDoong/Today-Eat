1e76457f0658e258a69cf942abc3bc784cc0f89f
junhyeok <junhyeok0246@naver.com>
2023-08-08 16:14:33 +0900

    가게 검색 API 예외처리
    검색어가 입력되지 않았을 때 400 에러 반환

--- src/models/stores/controller/index.js
+++ src/models/stores/controller/index.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { storeService } from "../service";
 import { UserService } from "../../users/service";
-import {storeData, campersData} from "../../../utils";
 
 class StoreController{
     router;
@@ -16,9 +15,6 @@ class StoreController{
     }
 
     init(){
-        this.router.get("/fetch-campers-data",campersData.bind(this));
-        this.router.get("/fetch-store-data",storeData.bind(this));
-        
         this.router.get("/rank-sample",this.getRankSample.bind(this));
         this.router.get("/rank",this.getRank.bind(this));
         this.router.get("/category",this.getStoreByCategory.bind(this));
@@ -149,6 +145,9 @@ class StoreController{
             let orderby = req.query.orderby;
             if(!orderby)orderby = "distance";
             const searchWord = req.query.keyword;
+
+            if(!searchWord)throw {status:400, message:"검색어를 입력해주세요."};
+
             const stores = await this.storeService.searchStore(user.campersId,searchWord,orderby);
 
             res.status(200).json(stores);
